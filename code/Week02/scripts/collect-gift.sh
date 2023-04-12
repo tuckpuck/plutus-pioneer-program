c71c039bd9b40df992a5b7260712a9aa19b5e814
Tucker <tuckertriggs@gmail.com>
2023-04-12 01:13:35 +0000

    update scripts for make-gift and collect-gift, add gift tx files

--- code/Week02/scripts/collect-gift.sh
+++ code/Week02/scripts/collect-gift.sh
@@ -1,46 +1,93 @@
 #!/bin/bash
 
-assets=/workspace/code/Week02/assets
-keypath=/workspace/keys
+assets=./workspace/code/Week02/assets
+scriptassets=./workspace/repo/code/Week02/040202/
+keypath=./workspace/repo/code/Week02/040202/keys/
 name="$1"
 collateral="$2"
 txin="$3"
 
-pp="$assets/protocol-parameters.json"
-body="$assets/collect-gift.txbody"
-tx="$assets/collect-gift.tx"
+pp="$scriptassets/protocol-parameters.json"
+body="$scriptassets/collect-gift.txbody"
+tx="$scriptassets/collect-gift.tx"
 
 # Query the protocol parameters \
 
 cardano-cli query protocol-parameters \
     --testnet-magic 2 \
-    --out-file "$pp"
+    --out-file "./workspace/repo/code/Week02/040202/protocol-parameters.json"
 
 # Build the transaction
 cardano-cli transaction build \
     --babbage-era \
     --testnet-magic 2 \
-    --tx-in "$txin" \
-    --tx-in-script-file "$assets/gift.plutus" \
+    --tx-in "0b91e9490ee12542eda1ea602f4bf8b8635f9c1d2b4e90bd4814f83b7501797a#0" \
+    --tx-in-script-file "./workspace/repo/code/Week02/assets/gift.plutus" \
     --tx-in-inline-datum-present \
-    --tx-in-redeemer-file "$assets/unit.json" \
-    --tx-in-collateral "$collateral" \
-    --change-address "$(cat "$keypath/$name.addr")" \
-    --protocol-params-file "$pp" \
-    --out-file "$body"
+    --tx-in-redeemer-file "./workspace/repo/code/Week02/assets/unit.json" \
+    --tx-in-collateral "b205bd2f94c6a4958562772f8672ad4f43eddb9c10856b38fabd6682cb9aca69#0" \
+    --change-address "addr_test1vqt9aq65jupw7exyg0h5xhglapje8rkmcvxrpklgz0utuxqjw94vq" \
+    --protocol-params-file "./workspace/repo/code/Week02/040202/protocol-parameters.json" \
+    --out-file "./workspace/repo/code/Week02/040202/collect-gift.txbody"
     
 # Sign the transaction
 cardano-cli transaction sign \
-    --tx-body-file "$body" \
-    --signing-key-file "$keypath/$name.skey" \
+    --tx-body-file "./workspace/repo/code/Week02/040202/collect-gift.txbody" \
+    --signing-key-file "./workspace/repo/code/Week02/040202/keys/userReceivingGift.skey" \
     --testnet-magic 2 \
-    --out-file "$tx"
+    --out-file "./workspace/repo/code/Week02/040202/collect-gift.tx"
 
 # Submit the transaction
 cardano-cli transaction submit \
     --testnet-magic 2 \
-    --tx-file "$tx"
+    --tx-file "./workspace/repo/code/Week02/040202/collect-gift.tx"
 
-tid=$(cardano-cli transaction txid --tx-file "$tx")
+tid=$(cardano-cli transaction txid --tx-file "./workspace/repo/code/Week02/040202/collect-gift.tx")
 echo "transaction id: $tid"
 echo "Cardanoscan: https://preview.cardanoscan.io/transaction/$tid"
+
+# ORIGINAL CODE
+# assets=/workspace/code/Week02/assets
+# keypath=/workspace/keys
+# name="$1"
+# collateral="$2"
+# txin="$3"
+
+# pp="$assets/protocol-parameters.json"
+# body="$assets/collect-gift.txbody"
+# tx="$assets/collect-gift.tx"
+
+# # Query the protocol parameters \
+
+# cardano-cli query protocol-parameters \
+#     --testnet-magic 2 \
+#     --out-file "$pp"
+
+# # Build the transaction
+# cardano-cli transaction build \
+#     --babbage-era \
+#     --testnet-magic 2 \
+#     --tx-in "$txin" \
+#     --tx-in-script-file "$assets/gift.plutus" \
+#     --tx-in-inline-datum-present \
+#     --tx-in-redeemer-file "$assets/unit.json" \
+#     --tx-in-collateral "$collateral" \
+#     --change-address "$(cat "$keypath/$name.addr")" \
+#     --protocol-params-file "$pp" \
+#     --out-file "$body"
+    
+# # Sign the transaction
+# cardano-cli transaction sign \
+#     --tx-body-file "$body" \
+#     --signing-key-file "$keypath/$name.skey" \
+#     --testnet-magic 2 \
+#     --out-file "$tx"
+
+# # Submit the transaction
+# cardano-cli transaction submit \
+#     --testnet-magic 2 \
+#     --tx-file "$tx"
+
+# tid=$(cardano-cli transaction txid --tx-file "$tx")
+# echo "transaction id: $tid"
+# echo "Cardanoscan: https://preview.cardanoscan.io/transaction/$tid"
--- code/Week02/scripts/make-gift.sh
+++ code/Week02/scripts/make-gift.sh
@@ -1,40 +1,103 @@
 #!/bin/bash
 
-assets=/workspace/code/Week02/assets
-keypath=/workspace/keys
-name="$1"
-txin="$2"
-body="$assets/gift.txbody"
-tx="$assets/gift.tx"
+# This is for creating user addressses, creating an address for the gift script, and sending money to script address
 
-# Build gift address 
-cardano-cli address build \
-    --payment-script-file "$assets/gift.plutus" \
-    --testnet-magic 2 \
-    --out-file "$assets/gift.addr"
+# Create userSendingGift keys
+path=./workspace/repo/code/Week02/040202/keys/
+mkdir -p "$path"
+
+vkey="$path/userSendingGift.vkey"
+skey="$path/userSendingGift.skey"
+addr="$path/userSendingGift.addr"
+
+cardano-cli address key-gen --verification-key-file "$vkey" --signing-key-file "$skey" &&
+cardano-cli address build --payment-verification-key-file "$vkey" --testnet-magic 2 --out-file "$addr"
+
+# Create userReceivingGift keys
+path=./workspace/repo/code/Week02/040202/keys/
+mkdir -p "$path"
+
+vkey="$path/userReceivingGift.vkey"
+skey="$path/userReceivingGift.skey"
+addr="$path/userReceivingGift.addr"
+
+cardano-cli address key-gen --verification-key-file "$vkey" --signing-key-file "$skey" &&
+cardano-cli address build --payment-verification-key-file "$vkey" --testnet-magic 2 --out-file "$addr"
+
+# Send test ada to UserSendingGift
+# Then, query using this command:
+cardano-cli query utxo --testnet-magic 2 --address addr_test1vp8u4t8jhs4zdmas3lk4twdtz3kcuh7n7lmu53m6wdzkncgy3k
+
+# Build gift address. Make a script address from the serialzed script. 
+cardano-cli address build 
+--payment-script-file "./workspace/repo/code/Week02/assets/gift.plutus" 
+--testnet-magic 2 
+--out-file "./workspace/repo/code/Week02/040202/gift.addr"
 
 # Build the transaction
 cardano-cli transaction build \
     --babbage-era \
     --testnet-magic 2 \
-    --tx-in "$txin" \
-    --tx-out "$(cat "$assets/gift.addr") + 3000000 lovelace" \
-    --tx-out-inline-datum-file "$assets/unit.json" \
-    --change-address "$(cat "$keypath/$name.addr")" \
-    --out-file "$body"
-    
+    --tx-in "0a7270ed42026bfda15e7c8343bef12f166aa86960864433d4a19c177ab51d71#0" \
+    --tx-out "$(cat "./workspace/repo/code/Week02/040202/gift.addr") + 3000000 lovelace" \
+    --tx-out-inline-datum-file "./workspace/repo/code/Week02/assets/unit.json" \
+    --change-address "$(cat "./workspace/repo/code/Week02/040202/keys/userSendingGift.addr")" \
+    --out-file "./workspace/repo/code/Week02/040202/gift.txbody"
+
+
 # Sign the transaction
 cardano-cli transaction sign \
-    --tx-body-file "$body" \
-    --signing-key-file "$keypath/$name.skey" \
+    --tx-body-file "./workspace/repo/code/Week02/040202/gift.txbody" \
+    --signing-key-file "./workspace/repo/code/Week02/040202/keys/userSendingGift.skey" \
     --testnet-magic 2 \
-    --out-file "$tx"
+    --out-file "./workspace/repo/code/Week02/040202/gift.tx"
 
 # Submit the transaction
 cardano-cli transaction submit \
     --testnet-magic 2 \
-    --tx-file "$tx"
+    --tx-file "./workspace/repo/code/Week02/040202/gift.tx"
 
-tid=$(cardano-cli transaction txid --tx-file "$tx")
+tid=$(cardano-cli transaction txid --tx-file "./workspace/repo/code/Week02/040202/gift.tx")
 echo "transaction id: $tid"
 echo "Cardanoscan: https://preview.cardanoscan.io/transaction/$tid"
+
+
+# ORIGINAL CODE
+# assets=/workspace/code/Week02/assets
+# keypath=/workspace/keys
+# name="$1"
+# txin="$2"
+# body="$assets/gift.txbody"
+# tx="$assets/gift.tx"
+
+# # Build gift address 
+# cardano-cli address build \
+#     --payment-script-file "$assets/gift.plutus" \
+#     --testnet-magic 2 \
+#     --out-file "$assets/gift.addr"
+
+# # Build the transaction
+# cardano-cli transaction build \
+#     --babbage-era \
+#     --testnet-magic 2 \
+#     --tx-in "$txin" \
+#     --tx-out "$(cat "$assets/gift.addr") + 3000000 lovelace" \
+#     --tx-out-inline-datum-file "$assets/unit.json" \
+#     --change-address "$(cat "$keypath/$name.addr")" \
+#     --out-file "$body"
+    
+# # Sign the transaction
+# cardano-cli transaction sign \
+#     --tx-body-file "$body" \
+#     --signing-key-file "$keypath/$name.skey" \
+#     --testnet-magic 2 \
+#     --out-file "$tx"
+
+# # Submit the transaction
+# cardano-cli transaction submit \
+#     --testnet-magic 2 \
+#     --tx-file "$tx"
+
+# tid=$(cardano-cli transaction txid --tx-file "$tx")
+# echo "transaction id: $tid"
+# echo "Cardanoscan: https://preview.cardanoscan.io/transaction/$tid"
